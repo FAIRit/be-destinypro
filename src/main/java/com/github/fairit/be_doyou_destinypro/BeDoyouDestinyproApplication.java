b4eefcc6a292ed91a16a85ac206fddad559edae8
Angelika Cechowska <angelika.cechowska@gmail.com>
2020-02-26 10:38:49 +0100

    init

---
+++ src/main/java/com/github/fairit/be_doyou_destinypro/BeDoyouDestinyproApplication.java
@@ -0,0 +1,13 @@
+package com.github.fairit.be_doyou_destinypro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeDoyouDestinyproApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeDoyouDestinyproApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/fairit/be_doyou_destinypro/BeDoyouDestinyproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fairit.be_doyou_destinypro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeDoyouDestinyproApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
