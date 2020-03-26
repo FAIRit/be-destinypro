1cd8c3e49d2912f4266ef31bc8a794c87df80455
Angelika Cechowska <angelika.cechowska@gmail.com>
2020-03-26 13:17:45 +0100

    Added character and player Api tests, added other test to main test class.

--- src/test/java/com/github/fairit/destinypro/BeDoyouDestinyproApplicationTests.java
+++ src/test/java/com/github/fairit/destinypro/BeDoyouDestinyproApplicationTests.java
@@ -1,10 +1,12 @@
 package com.github.fairit.destinypro;
 
 import com.github.fairit.destinypro.controller.PlayerControllerTest;
+import com.github.fairit.destinypro.service.character_service.CharacterApiServiceTest;
+import com.github.fairit.destinypro.service.player_service.PlayerApiServiceTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({PlayerControllerTest.class})
+@Suite.SuiteClasses({PlayerControllerTest.class, CharacterApiServiceTest.class, PlayerApiServiceTest.class})
 public class BeDoyouDestinyproApplicationTests {
 }
---
+++ src/test/java/com/github/fairit/destinypro/service/player_service/PlayerApiServiceTest.java
@@ -0,0 +1,32 @@
+package com.github.fairit.destinypro.service.player_service;
+
+
+import com.github.fairit.destinypro.dto.player.api.PlayerApi;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PlayerApiServiceTest {
+
+    @Autowired
+    private PlayerApiService playerApiService;
+
+    @Test
+    public void findPlayerApiByNicknameByNicknameShouldReturnCorrectedPlayerJson() {
+
+        PlayerApi playerByNickname = playerApiService.findPlayerApiByNickname("katojido");
+
+        assertThat(playerByNickname, notNullValue());
+        assertThat(playerByNickname.getResponse().get(0).getDisplayName(), containsString("katojido"));
+        assertThat(playerByNickname.getResponse().get(0).getMembershipId(), containsString("4611686018485400803"));
+        assertThat(playerByNickname.getResponse().get(0).getMembershipId(), containsString("3"));
+        assertThat(playerByNickname, instanceOf(PlayerApi.class));
+    }
+}
