89d561d74c980e3c44ad03e9775b04e1114ef53b
Angelika Cechowska <angelika.cechowska@gmail.com>
2020-03-30 10:00:05 +0200

    Added PlayerServiceTest, added maven dependencies for checking corrected JSON.

--- src/test/java/com/github/fairit/destinypro/BeDoyouDestinyproApplicationTests.java
+++ src/test/java/com/github/fairit/destinypro/BeDoyouDestinyproApplicationTests.java
@@ -3,10 +3,12 @@ package com.github.fairit.destinypro;
 import com.github.fairit.destinypro.controller.PlayerControllerTest;
 import com.github.fairit.destinypro.service.character_service.CharacterApiServiceTest;
 import com.github.fairit.destinypro.service.player_service.PlayerApiServiceTest;
+import com.github.fairit.destinypro.service.player_service.PlayerServiceTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({PlayerControllerTest.class, CharacterApiServiceTest.class, PlayerApiServiceTest.class})
+@Suite.SuiteClasses({PlayerControllerTest.class, CharacterApiServiceTest.class, PlayerApiServiceTest.class,
+        PlayerServiceTest.class})
 public class BeDoyouDestinyproApplicationTests {
 }
