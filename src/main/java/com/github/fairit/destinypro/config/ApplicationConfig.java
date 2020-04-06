1da5937c548478981820d38a55b551eb7d8debf8
angelika-c <angelika.cechowska@gmail.com>
2020-04-06 08:54:08 +0200

    Transferred @Beans to ApplicationConfig class.

--- src/main/java/com/github/fairit/destinypro/config/ApplicationConfig.java
+++ src/main/java/com/github/fairit/destinypro/config/ApplicationConfig.java
@@ -10,15 +10,14 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class ApplicationConfig {
 
-    private final HttpHeaders httpHeaders;
-
-    public ApplicationConfig(final HttpHeaders httpHeaders) {
-        this.httpHeaders = httpHeaders;
+    public HttpEntity getHttpEntity() {
+        getHttpHeaders().set("x-api-key", "188bb3eb49644d198f0111661ebb8f69");
+        return new HttpEntity(getHttpHeaders());
     }
 
-    public HttpEntity getHttpEntity() {
-        httpHeaders.set("x-api-key", "188bb3eb49644d198f0111661ebb8f69");
-        return new HttpEntity(httpHeaders);
+    @Bean
+    public HttpHeaders getHttpHeaders() {
+        return new HttpHeaders();
     }
 
     @Bean
@@ -26,10 +25,6 @@ public class ApplicationConfig {
         return new RestTemplate();
     }
 
-    @Bean
-    public HttpHeaders getHttpHeaders() {
-        return new HttpHeaders();
-    }
     @Bean
     public ModelMapper getModelMapper() {
         return new ModelMapper();
