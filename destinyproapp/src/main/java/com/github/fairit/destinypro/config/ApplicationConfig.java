d9bd6ba4dec24a2c5ef775347b66eebdd38abc20
angelika-c <angelika.cechowska@gmail.com>
2020-04-15 15:08:16 +0200

    Added CORS configure.

--- destinyproapp/src/main/java/com/github/fairit/destinypro/config/ApplicationConfig.java
+++ destinyproapp/src/main/java/com/github/fairit/destinypro/config/ApplicationConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ApplicationConfig {
@@ -18,6 +20,17 @@ public class ApplicationConfig {
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.set("x-api-key", apiKey);
         return new HttpEntity(httpHeaders);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/findplayer")
+                        .allowedOrigins("http://localhost:8080");
+            }
+        };
     }
 
     @Bean
